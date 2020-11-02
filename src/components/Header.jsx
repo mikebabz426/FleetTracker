b050fac66a1b212aafa0a1fa21240cd7f5cc549b
mikebabz426 <60781512+mikebabz426@users.noreply.github.com>
2020-11-02 08:46:33 -0600

    added fuel price fetching from EIA.gov

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppBar, Toolbar, Typography, Fab, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import AddIcon from "@material-ui/icons/Add";
@@ -8,6 +8,7 @@ import Filter from "./Filters/Filter";
 
 const Header = (props) => {
 	const [filters, setFilters] = useContext(FilterContext);
+	const [fuelPrice, setFuelPrice] = useState(0);
 	const classes = useStyles();
 	const teamOptions = ["All", "Team One", "Team Two", "Team Three"];
 	const dayOptions = ["All", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
@@ -18,12 +19,26 @@ const Header = (props) => {
 			: setFilters({ ...filters, team: e.target.value });
 	};
 
+	const getFuelPrice = async () => {
+		await fetch(
+			`http://api.eia.gov/series/?api_key=5c7387cb68efe9616f46194d17493e35&series_id=PET.EMD_EPD2D_PTE_NUS_DPG.W`
+		)
+			.then((res) => res.json())
+			.then((data) => setFuelPrice(data.series[0].data[0][1]));
+	};
+
+	getFuelPrice();
+
 	return (
 		<AppBar color="primary" position="fixed" className={classes.appBar}>
 			<Toolbar className={classes.toolbar}>
 				<Typography className={classes.typographyStyles} variant="h6">
 					My Fleet Tracker
 				</Typography>
+				<Typography className={classes.typographyStyles} variant="body2">
+					US Diesel Avg: ${fuelPrice}
+				</Typography>
+
 				<Box className={classes.box}>
 					<Typography>Team Filter: </Typography>
 					<Filter
